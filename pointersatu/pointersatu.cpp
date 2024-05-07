6ded1277fd784bcad75ed2a8e6fd0394756ca267
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-07 10:32:18 +0700

    Add project files.

---
+++ pointersatu/pointersatu.cpp
@@ -0,0 +1,15 @@
+// pointersatu.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+   
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
